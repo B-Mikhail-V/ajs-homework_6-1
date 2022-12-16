078cc4593cd9c278ad4751cae6933f6f35e64fb3
Mikail Bakulin <bakulin.mv@gmail.com>
2022-12-16 10:05:55 +0300

    Corr_Task1

--- src/app.js
+++ src/app.js
@@ -1,22 +1,17 @@
-/* eslint no-prototype-builtins: "error" */
-const objIn = {
-  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-};
-
 export default function orderByProps(obj, params) {
   const kk = Object.keys(obj); // все ключи исходного объекта
   const firstArray = params; // новый список ключей
   const objNew = {};
-  kk.splice(kk.lastIndexOf(params[0]), 1); // удаление первого параметра из списка ключей
-  kk.splice(kk.lastIndexOf(params[1]), 1); // удаление второго параметра из списка ключей
+  /* eslint-disable-next-line */
+  for (const id in params) {
+    kk.splice(kk.lastIndexOf(params[id]), 1); // удаление параметров из списка ключей
+  }
 
   // сортировка оставшего списка ключей и добавление к новому списку ключей
   kk.sort();
   /* eslint-disable-next-line */
   for (const item in kk) {
-    // if (kk.hasOwnProperty(item)) {
     firstArray.push(kk[item]);
-    // }
   }
 
   // формирование нового объекта
